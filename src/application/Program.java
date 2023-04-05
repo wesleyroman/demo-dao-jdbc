02553d4866fd9efa1cf906db276bf6327c98c0a1
Wesley Souza Roman <wesley.sdesouza@gmail.com>
2023-04-05 14:33:50 -0300

    updated implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -37,6 +37,12 @@ public class Program {
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
 
+		System.out.println("=== TEST 5: seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Marta Wayne");
+		sellerDao.update(seller);
+		System.out.println("Updated Completed");
+
 
 	}
 
